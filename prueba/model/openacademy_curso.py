cfab9aed3538128fac9b55cec6e78d39b253a6e6
Jose Luis Ortiz Gomez <jlortizgomez1@gmail.com>
2017-08-03 10:12:08 -0500

    agregado con visual

--- prueba/model/openacademy_curso.py
+++ prueba/model/openacademy_curso.py
@@ -1,12 +1,12 @@
 from manifest import models, fields
-
 '''
 Este es la creacion del curso
 '''
-clas Course(models,Model):
-    '''
-    Esta es la creacion de la clase
-    '''
+clas Course(models, Model):
+'''
+Esta es la creacion de la clase
+'''
     _name = 'openacademy.course'
-    name = fields.Char(string="Title", required=True) #Creacion del campo field
+    # Creacion del campo field
+    name = fields.Char(string="Title", required=True)
     description = fields.Text(string="Description")
