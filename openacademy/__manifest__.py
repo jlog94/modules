a975133451fa88b16c204c095855501a2f56d2b8
Jose Luis Ortiz Gomez <jlortizgomez1@gmail.com>
2017-08-03 11:16:56 -0500

    depurar codigo

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -2,20 +2,20 @@
 {
     'name': "Open Academy",
 
-    'summary': """Manage trainings""",
+    'summary': """Prueba del Curso""",
 
     'description': """
-		Favor de atenderlos
+        Practicando con Odoo
     """,
 
-    'author': "Soluciones 4G",
+    'author': "soluciones4g",
     'website': "http://www.soluciones4g.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
-    'category': 'PRO',
-    'version': '0.1.1r',
+    'category': 'Test',
+    'version': '0.1r',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
@@ -23,12 +23,13 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'templates.xml',
+        #'views/views.xml',
+        # 'views/templates.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
-        'demo.xml',
+        #'demo/demo.xml',
     ],
-    'installable':True,
-    'auto_install':False,
+    'intallable': True,
+    'auto_install': False,
 }
