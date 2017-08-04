2a7fc713efe20bb60ff1e4fdf65f69ac4d73782e
Jose Luis Ortiz Gomez <jlortizgomez1@gmail.com>
2017-08-04 12:07:12 -0500

    [REF]: session (se agrego archivo py)

---
+++ openacademy/models/openacademy_session.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields
+
+name = field.Char(string="Title", required=True)
+description = fields.Text()
+
+
+class Session(models.Model):
+    _name = 'openacademy.session'
+
+    name = fields.Char(required=True)
+    start_date = fields.Date()
+    duration = fields.Float(digits=(6, 2), help="Duration in days")
+    seats = fields.Integer(string=Number of seats)
