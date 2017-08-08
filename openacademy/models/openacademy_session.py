afccf7a746a4f9d75aeeddeb09a0eb7f0ed46b52
Jose Luis Ortiz Gómez <jlortizgomez1@gmail.com>
2017-08-08 09:38:00 -0500

    Se agrego el dominio de Busqueda ilike

--- openacademy/models/openacademy_session.py
+++ openacademy/models/openacademy_session.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields
 
-name = fields.Char(string="Title", required=True)
-description = fields.Text()
+#name = fields.Char(string="Title", required=True)
+#description = fields.Text()
 
 
 class Session(models.Model):
@@ -12,7 +12,9 @@ class Session(models.Model):
     start_date = fields.Date(string="Fecha")
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
-    instructor_id = fields.Many2one('res.partner', string="Instructor")
+    instructor_id = fields.Many2one('res.partner', string="Instructor",
+                                    domain=['|',("instructor", "=", True),
+                                            ('category_id.name', 'ilike', "Teacher")])
     course_id = fields.Many2one('openacademy.curso',
                                 ondelete='cascade', string="Course", required=True)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
