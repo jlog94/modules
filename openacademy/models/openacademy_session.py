ce05a2816189288fc8717f0444159f5aa7a410b6
Jose Luis Ortiz Gómez <jlortizgomez1@gmail.com>
2017-08-09 09:15:21 -0500

    Se agregaron los partner

--- openacademy/models/openacademy_session.py
+++ openacademy/models/openacademy_session.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields
+from odoo import api, models, fields
 
 #name = fields.Char(string="Title", required=True)
 #description = fields.Text()
@@ -9,12 +9,22 @@ class Session(models.Model):
     _name = 'openacademy.session'
 
     name = fields.Char(string="Name", required=True)
-    start_date = fields.Date(string="Fecha")
+    start_date = fields.Date(default=fields.Date.today)
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
     instructor_id = fields.Many2one('res.partner', string="Instructor",
-                                    domain=['|',("instructor", "=", True),
+                                    domain=['|', ("instructor", "=", True),
                                             ('category_id.name', 'ilike', "Teacher")])
     course_id = fields.Many2one('openacademy.curso',
                                 ondelete='cascade', string="Course", required=True)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
+    taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
+    active= fields.Boolean(default="True")
+
+    @api.one
+    @api.depends('seats', 'attendee_ids')
+    def _taken_seats(self):
+        if not self.seats:
+            self.taken_seats = 0.0
+        else:
+            self.taken_seats = 100.0 * len(self.attendee_ids) / self.seats
