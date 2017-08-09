f740076c691a1a5f3816b531f1193d6fb904698c
Jose Luis Ortiz Gómez <jlortizgomez1@gmail.com>
2017-08-09 18:08:55 -0500

    calendar

--- openacademy/models/openacademy_curso.py
+++ openacademy/models/openacademy_curso.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class Curso (models.Model):
@@ -8,6 +8,29 @@ class Curso (models.Model):
     # Field reserved to identified name re
     name = fields.Char(string='Title', required=True)
     description = fields.Text(string='Description')
-    responsible_id = fields.Many2one(
-        'res.users', ondelete='set null', string="Responsible", index=True)
-    session_ids=fields.One2many('openacademy.session','course_id',string="Sessions")
+    responsible_id = fields.Many2one('res.users', ondelete='set null', string="Responsible", index=True)
+    session_ids = fields.One2many('openacademy.session', 'course_id', string="Sessions")
+
+    @api.multi
+    def copy(self, default=None):
+        default = dict(default or {})
+
+        copied_count = self.search_count(
+            [('name', '=like', u"Copy of {}%".format(self.name))])
+        if not copied_count:
+            new_name = u"Copy of {}".format(self.name)
+        else:
+            new_name = u"Copy of {} ({})".format(self.name, copied_count)
+
+        default['name'] = new_name
+        return super(Curso, self).copy(default)
+    
+    _sql_constraints = [
+        ('name_description_check',
+         'CHECK(name != description)',
+         "El titulo del curso no debe ser igual a la descripcion"),
+
+        ('name_unique',
+         'UNIQUE(name)',
+         "El nombre del curso debe ser unico"),
+    ]
--- openacademy/models/openacademy_session.py
+++ openacademy/models/openacademy_session.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from datetime import timedelta
 from odoo import api, models, fields
 
 #name = fields.Char(string="Title", required=True)
@@ -19,7 +20,11 @@ class Session(models.Model):
                                 ondelete='cascade', string="Course", required=True)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
     taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
-    active= fields.Boolean(default="True")
+    end_date = fields.Date(string="End Date", store=True,
+        compute='_get_end_date', inverse='_set_end_date')
+    active = fields.Boolean(default="True")
+
+
 
     @api.one
     @api.depends('seats', 'attendee_ids')
@@ -28,3 +33,51 @@ class Session(models.Model):
             self.taken_seats = 0.0
         else:
             self.taken_seats = 100.0 * len(self.attendee_ids) / self.seats
+
+    @api.onchange('seats', 'attendee_ids')
+    def _verify_valid_seats(self):
+        if self.seats < 0:
+            return {
+                'warning': {
+                    'title': "Incorrect 'seats' value",
+                    'message': "The number of available seats may not be negative",
+                },
+            }
+        if self.seats < len(self.attendee_ids):
+            return {
+                'warning': {
+                    'title': "Too many attendees",
+                    'message': "Increase seats or remove excess attendees",
+                },
+            }
+
+    @api.constrains('instructor_id', 'attendee_ids')
+    def _check_instructor_not_in_attendees(self):
+        for r in self:
+            if r.instructor_id and r.instructor_id in r.attendee_ids:
+                raise exceptions.ValidationError(
+                    "A session's instructor can't be an attendee")
+    
+    @api.depends('start_date', 'duration')
+    def _get_end_date(self):
+        for r in self:
+            if not (r.start_date and r.duration):
+                r.end_date = r.start_date
+                continue
+
+            # Add duration to start_date, but: Monday + 5 days = Saturday, so
+            # subtract one second to get on Friday instead
+            start = fields.Datetime.from_string(r.start_date)
+            duration = timedelta(days=r.duration, seconds=-1)
+            r.end_date = start + duration
+
+    def _set_end_date(self):
+        for r in self:
+            if not (r.start_date and r.end_date):
+                continue
+
+            # Compute the difference between dates, but: Friday - Monday = 4 days,
+            # so add one day to get 5 days instead
+            start_date = fields.Datetime.from_string(r.start_date)
+            end_date = fields.Datetime.from_string(r.end_date)
+            r.duration = (end_date - start_date).days + 1
