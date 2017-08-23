c29aaebd1f79f65dc4fc5ca4b9fa518cb4d3c339
Jose Luis Ortiz Gómez <jlortizgomez1@gmail.com>
2017-08-23 13:26:45 -0500

    Se renombro archivos Openacademy_course y session_report

---
+++ openacademy/models/openacademy_course.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api, _
+
+
+class Course (models.Model):
+    _name = 'openacademy.course'  # Model odoo name
+
+    # Field reserved to identified name re
+    name = fields.Char(string='Title', required=True)
+    description = fields.Text(string='Description')
+    responsible_id = fields.Many2one('res.users', ondelete='set null', string="Responsible", index=True)
+    session_ids = fields.One2many('openacademy.session', 'course_id', string="Sessions")
+
+    @api.multi
+    def copy(self, default=None):
+        default = dict(default or {})
+
+        copied_count = self.search_count(
+            [('name', '=like', u"Copy of {}%".format(self.name))])
+        if not copied_count:
+            new_name = _(u"Copy of {}").format(self.name)
+        else:
+            new_name = _(u"Copy of {} ({})").format(self.name, copied_count)
+
+        default['name'] = new_name
+        return super(Course, self).copy(default)
+    
+    _sql_constraints = [
+        ('name_description_check',
+         'CHECK(name != description)',
+         _("El titulo del curso no debe ser igual a la descripcion")),
+
+        ('name_unique',
+         'UNIQUE(name)',
+         _("El nombre del curso debe ser unico")),
+    ]
